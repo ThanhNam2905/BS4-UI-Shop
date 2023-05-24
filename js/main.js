0f9b7a69a40988529dc680518e2a04d423a4d7c2
nam <you@example.com>
2023-05-24 16:04:29 +0700

    Add section Features and Products

--- js/main.js
+++ js/main.js
@@ -1,5 +1,38 @@
 
+// ---------------- jQuery ----------------------
+$(document).ready(function () {
+    // Initiate the wowjs
+    new WOW().init();
 
+    // Owl carousel
+    $('#products-carousel').owlCarousel({
+        loop:true,
+        margin:0,
+        nav:true,
+        dots: false,
+        autoplay: true,
+        smartSpeed: 1000,
+        responsive:{
+            0:{
+                items:1
+            },
+            600:{
+                items:2
+            },
+            992:{
+                items:3
+            },
+            1280:{
+                items:4
+            }
+        }
+    })
+})
+
+
+
+
+// ---------------- Javascript ----------------------
 const navLinks = document.querySelectorAll('.header #collapseNavbarMenu .navbar-nav .nav-item .nav-link');
 
 if (navLinks) {
@@ -10,3 +43,4 @@ if (navLinks) {
         })
     })
 }
+
